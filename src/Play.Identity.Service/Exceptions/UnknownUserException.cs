2245d899497b5cae3296707fa1c1368e366d783c
alvperagu <alvaropa93@gmail.com>
2024-09-04 17:55:57 +0200

    Update UnknownUserException.cs

--- src/Play.Identity.Service/Exceptions/UnknownUserException.cs
+++ src/Play.Identity.Service/Exceptions/UnknownUserException.cs
@@ -5,22 +5,11 @@ namespace Play.Identity.Service.Exceptions;
 [Serializable]
 internal class UnknownUserException : Exception
 {
-    private Guid userId;
-
-    public UnknownUserException()
-    {
-    }
-
-    public UnknownUserException(Guid userId)
+    public UnknownUserException(Guid userId) 
+    :base($"Unknown user '{userId}'")
     {
         this.userId = userId;
     }
 
-    public UnknownUserException(string message) : base(message)
-    {
-    }
-
-    public UnknownUserException(string message, Exception innerException) : base(message, innerException)
-    {
-    }
+    public Guid userId {get;}
 }
